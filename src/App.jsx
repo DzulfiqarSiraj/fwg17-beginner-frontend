cdc46cda4b15d6cd2bd8eab6390d1951f1f18135
DzulfiqarSiraj <103058254+DzulfiqarSiraj@users.noreply.github.com>
2023-12-13 17:12:28 +0700

    uncomment entry point router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-// import Home from './pages/Home'
+import Home from './pages/Home'
 import Login from './pages/Login'
 
 const router = createBrowserRouter([
-    // {
-    //     path: '/',
-    //     element: <Home />
-    // },
+    {
+        path: '/',
+        element: <Home />
+    },
     {
         path: '/login',
         element: <Login />
