dd02956e5cc653451bb9a84ff648eff1e831b7ff
DzulfiqarSiraj <103058254+DzulfiqarSiraj@users.noreply.github.com>
2023-12-20 09:17:06 +0700

    add component

---
+++ src/components/AddressInput.jsx
@@ -0,0 +1,18 @@
+import { FiMapPin } from "react-icons/fi";
+
+const AddressInput = () => {
+    return (
+        <label htmlFor="address" className="flex flex-col gap-2">
+            <span className="font-semibold text-lg">Address</span>
+            <div className="flex flex-row items-center border h-14 border-gray-300 rounded-lg px-4 gap-4">
+                <div>
+                    <FiMapPin className="text-gray-500" />
+                </div>
+                <input className="flex-1 outline-none placeholder:text-sm placeholder:text-gray-500" id="address" type="text" name="address" placeholder="Enter Your Address" autoComplete="on"/>
+            </div>
+        </label>
+    )
+
+}
+
+export default AddressInput
