367ed9c739572f27a870c733a6a7d2d09d6d5690
DzulfiqarSiraj <103058254+DzulfiqarSiraj@users.noreply.github.com>
2023-12-22 08:40:01 +0700

    update component

--- src/components/FullNameInput.jsx
+++ src/components/FullNameInput.jsx
@@ -3,13 +3,13 @@ import { FiUser } from "react-icons/fi";
 const FullNameInput = () => {
 
     return (
-        <label htmlFor="fullname" className="flex flex-col gap-2">
+        <label htmlFor="fullName" className="flex flex-col gap-2">
             <span className="font-semibold text-lg">Full Name</span>
             <div className="flex flex-row items-center border h-14 border-gray-300 rounded-lg px-4 gap-4">
                 <div>
                     <FiUser className="text-gray-500" />
                 </div>
-                <input className="flex-1 outline-none placeholder:text-sm placeholder:text-gray-500" id="fullname" type="text" name="fullname" placeholder="Enter Your Full Name" autoComplete="on"/>
+                <input className="flex-1 outline-none placeholder:text-sm placeholder:text-gray-500" id="fullName" type="text" name="fullName" placeholder="Enter Your Full Name" autoComplete="on"/>
             </div>
         </label>
     )
