4a6307466ced1ccece012f0f1696fb5556c05e2c
DzulfiqarSiraj <103058254+DzulfiqarSiraj@users.noreply.github.com>
2023-12-28 08:11:11 +0700

    error handling register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -40,6 +40,12 @@ const Register = () => {
             if(!password){
                 throw new Error('password undefined')
             }
+            if(!confirmpassword){
+                throw new Error('confirm password undefined')
+            }
+            if(confirmpassword !== password){
+                throw new Error('password unmatch')
+            }
 
 
             const {data} = await axios.post('http://localhost:8888/auth/register', form.toString())
@@ -49,81 +55,57 @@ const Register = () => {
                     setSuccessInfo(<div className='flex flex-1 text-green-700'>{data.message}</div>)
                     setTimeout(()=>{
                         window.location = '/login'
-                    },2000)
+                    },3000)
                 }
             }
         } catch(err){
             if(err.message === 'input empty') {
+                setFullNameInfo(<div className='text-red-800 text-sm'>Name must not be empty!</div>)
                 setEmailInfo(<div className='text-red-800 text-sm'>Email must not be empty!</div>)
                 setPasswordInfo(<div className='text-red-800 text-sm'>Password must not be empty!</div>)
                 setTimeout(()=>{
+                    setFullNameInfo('')
                     setEmailInfo('')
                     setPasswordInfo('')
-                },2000)
+                },3000)
             }
             if(err.message === 'fullname undefined'){
                 setFullNameInfo(<div className='text-red-800 text-sm'>Name must not be empty!</div>)
                 setTimeout(()=>{
                     setFullNameInfo('')
-                },2000)
+                },3000)
             }
             if(err.message === 'email undefined'){
                 setEmailInfo(<div className='text-red-800 text-sm'>Email must not be empty!</div>)
                 setTimeout(()=>{
                     setEmailInfo('')
-                },2000)
+                },3000)
             }
             if(err.message === 'password undefined'){
                 setPasswordInfo(<div className='text-red-800 text-sm'>Password must not be empty!</div>)
                 setTimeout(()=>{
                     setPasswordInfo('')
-                },2000)
+                },3000)
             }
-            if(err.response.status == 401){
-                setSuccessInfo(<div className='text-red-800 text-sm'>{err.response.data.message}</div>)
+            if(err.message === 'confirm password undefined'){
+                setConfirmPasswordInfo(<div className='text-red-800 text-sm'>Confirm Password!</div>)
                 setTimeout(()=>{
-                    setSuccessInfo('')
-                },2000)
+                    setConfirmPasswordInfo('')
+                },3000)
             }
-        }
-
-
-        if(!fullName){
-            setFullNameInfo(<div className='text-red-800 text-sm'>Name must not be empty!</div>)
-            setTimeout(()=>{
-                setFullNameInfo('')
-            },2000)
-        }
-        if(!email){
-            setEmailInfo(<div className='text-red-800 text-sm'>Email must not be empty!</div>)
-            setTimeout(()=>{
-                setEmailInfo('')
-            },2000)
-        }
-        if(!password){
-            setPasswordInfo(<div className='text-red-800 text-sm'>Password must not be empty!</div>)
-            setTimeout(()=>{
-                setPasswordInfo('')
-            },2000)
-        }
-        if(!confirmpassword){
-            setConfirmPasswordInfo(<div className='text-red-800 text-sm'>Confirm Password!</div>)
-            setTimeout(()=>{
-                setConfirmPasswordInfo('')
-            },2000)
-        } else if(confirmpassword !== password){
-            setConfirmPasswordInfo(<div className='text-red-800 text-sm'>Password is unmatch</div>)
-            setTimeout(()=>{
-                setConfirmPasswordInfo('')
-            },2000)
-        }
-
-        if(fullName && email && password && (confirmpassword === password)){
-            setSuccessInfo(<div className='text-green-800 text-sm'>Registration Successfully</div>)
-            setTimeout(() => {
-                window.open('http://localhost:5173/login')
-            },2000)
-        }
+            if(err.message === 'password unmatch'){
+                setConfirmPasswordInfo(<div className='text-red-800 text-sm'>Unmatch Password!</div>)
+                setTimeout(()=>{
+                    setConfirmPasswordInfo('')
+                },3000)
+            }
+            if(fullName && email && password && (confirmpassword === password)){
+                setSuccessInfo(<div className='text-green-800 text-sm'>Registration Successfully</div>)
+                setTimeout(() => {
+                    window.open('http://localhost:5173/login')
+                },3000)
+            }
+        }        
     }
 
     return (
@@ -162,7 +144,7 @@ const Register = () => {
                         </div>
                         <div className="flex flex-col md:flex-row flex-1 items-center justify-center gap-4 rounded-2xl p-5 shadow-md hover:opacity-80 cursor-pointer active:scale-95 transition-all duration-300 active:shadow-none">
                             <div className="flex">
-                                <svg className="w-10 md:w-4" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100%" height="100%" viewBox="0 0 48 48">
+                                <svg className="w-10 md:w-4" xmlns="http://www.w3.org/3000/svg" x="0px" y="0px" width="100%" height="100%" viewBox="0 0 48 48">
                                 <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path><path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path><path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path><path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
                                 </svg>
                             </div>
